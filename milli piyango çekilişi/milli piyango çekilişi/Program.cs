5370f7b66c5371e788c83ab8d061c8c71b82c9af
Semhsen <semhsen@gmail.com>
2019-12-01 21:23:05 +0300

    Examples

---
+++ milli piyango çekilişi/milli piyango çekilişi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace milli_piyango_çekilişi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rastgele = new Random();
+            int a;
+            int b;
+            int c;
+            int d;
+            a = rastgele.Next(10);
+            b = rastgele.Next(10);
+            while (true)
+            {
+                Console.WriteLine("birinci sayıyı giriniz:");
+                c = Convert.ToInt32(Console.ReadLine());
+                if (c < 10 && c > -1) break;
+            }                                                                               
+            Console.WriteLine("ikinci sayıyı giriniz:");
+            d = Convert.ToInt32(Console.ReadLine());
+            if (a == c) Console.WriteLine("birinci sayıyı bildin");
+            if (b == d) Console.WriteLine("ikinci sayıyı bildin");
+            else Console.WriteLine("ikisinide bilemezdiniz");
+            Console.ReadKey();
+        }
+    }
+}
